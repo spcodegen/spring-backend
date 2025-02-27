e2d8a70b68825b3c434527bc1515befc4ce0a90f
spcodegen <shehansp2000@gmail.com>
2025-02-27 11:02:39 +0530

    add customer service full

--- src/main/java/edu/icet/ecom/controller/CustomerController.java
+++ src/main/java/edu/icet/ecom/controller/CustomerController.java
@@ -3,7 +3,8 @@ package edu.icet.ecom.controller;
 import edu.icet.ecom.dto.Customer;
 import edu.icet.ecom.service.CustomerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,10 @@ public class CustomerController {
 
     final CustomerService service;
 
-    @GetMapping("/add")
-    public void addCustomer(Customer customer){
+    @PostMapping("/add")
+    public void addCustomer(@RequestBody Customer customer){
         service.addCustomer(customer);
+        System.out.println(customer);
     }
 
 }
--- src/main/java/edu/icet/ecom/service/impl/CustomerServiceImpl.java
+++ src/main/java/edu/icet/ecom/service/impl/CustomerServiceImpl.java
@@ -1,13 +1,22 @@
 package edu.icet.ecom.service.impl;
 
 import edu.icet.ecom.dto.Customer;
+import edu.icet.ecom.entity.CustomerEntity;
+import edu.icet.ecom.repository.CustomerRepository;
 import edu.icet.ecom.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
+
+    final CustomerRepository repository;
+    final ModelMapper mapper;
+
     @Override
     public void addCustomer(Customer customer) {
-
+        repository.save(mapper.map(customer, CustomerEntity.class));
     }
 }
